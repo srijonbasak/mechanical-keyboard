023dd5f6242052a87990f6895b0da1cb1dc5839c
Srijon Basak <37615010+srijonbasak@users.noreply.github.com>
2022-05-19 13:11:29 +0600

    Update prototype2.ino

--- prototype-V2/prototype2.ino
+++ prototype-V2/prototype2.ino
@@ -1,27 +1,29 @@
 #include <Keypad.h>
 
-const byte ROWS = 3;
-const byte COLS = 10;
+const byte row = 3;
+const byte col = 10;
 
-char hexaKeys[ROWS][COLS] = {
-	{'q','w','e','r','t','y','u','i','o','p'},
-	{'a','s','d','f','g','h','j','k','l','-'},
-	{'z','x','c','v','b','n','m','-','-','-'}
-};
- 
-byte rowPins[ROWS] = {A2, A1, A0};
-byte colPins[COLS] = {3,4,5,6,7,8,9,10,11,12};
- 
-Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
- 
+char keys[row][col] = {
+  {'q','w','e','r','t','y','u','i','o','p'},
+  {'a','s','d','f','g','h','j','k','l',' '},
+  {'z','x','c','v','b','n','m','@','#','$'}
+  };
+
+byte rowPins[row] = {A0,A1,A2};
+byte colPins[col] = {2,3,4,5,6,7,8,9,10,11};
+
+Keypad ckeypad = Keypad(makeKeymap(keys),rowPins, colPins, row, col);
+
 void setup() {
   Serial.begin(9600);
+
 }
- 
+
 void loop() {
-  char customKey = customKeypad.getKey();
- 
-  if (customKey) {
-    Serial.println(customKey);
-  }
+  char ckey ;
+  ckey = ckeypad.getKey();
+  if (ckey){
+    Serial.print(ckey);
+    }
+
 }
